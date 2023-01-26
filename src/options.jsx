ad00e6033b1467a5137bd5560587b29bf84cd4a7
Diego H. Gurpegui <diego.gurpegui@improvein.com>
2023-01-26 20:25:57 -0300

    Preferred relays

--- src/options.jsx
+++ src/options.jsx
@@ -1,6 +1,7 @@
 import browser from 'webextension-polyfill'
-import React, {useState, useEffect} from 'react'
+import React, {useState, useCallback, useEffect} from 'react'
 import {render} from 'react-dom'
+import {useDebouncedCallback} from 'use-debounce'
 import {generatePrivateKey} from 'nostr-tools'
 
 import {Alert} from './alert'
@@ -13,13 +14,29 @@ import manifest from './manifest.json'
 
 function Options() {
   let [key, setKey] = useState('')
+  let [relays, setRelays] = useState([])
+  let [newRelayURL, setNewRelayURL] = useState('')
   let [permissions, setPermissions] = useState()
   let [message, setMessage] = useState('')
   let [messageType, setMessageType] = useState('info')
 
   useEffect(() => {
-    browser.storage.local.get(['private_key']).then(results => {
+    saveRelays()
+  }, [relays])
+
+  useEffect(() => {
+    browser.storage.local.get(['private_key', 'relays']).then(results => {
       if (results.private_key) setKey(results.private_key)
+      if (results.relays) {
+        let relaysList = []
+        for (let url in results.relays) {
+          relaysList.push({
+            url,
+            policy: results.relays[url]
+          })
+        }
+        setRelays(relaysList)
+      }
     })
   }, [])
 
@@ -38,6 +55,25 @@ function Options() {
     })
   }, [])
 
+  const showMessage = useCallback((msg, type = 'info', timeout = 3000) => {
+    setMessageType(type)
+    setMessage(msg)
+    if (timeout > 0) {
+      setTimeout(setMessage, 3000)
+    }
+  })
+
+  const saveRelays = useDebouncedCallback(async () => {
+    await browser.storage.local.set({
+      relays: Object.fromEntries(
+        relays
+          .filter(({url}) => url.trim() !== '')
+          .map(({url, policy}) => [url.trim(), policy])
+      )
+    })
+    showMessage('saved relays!')
+  }, 700)
+
   async function savePrivateKey(key) {
     setKey(key)
 
@@ -45,12 +81,9 @@ function Options() {
       await browser.storage.local.set({
         private_key: key
       })
-      setMessageType('success')
-      setMessage('Key saved!')
-      setTimeout(setMessage, 3000) // clean message
+      showMessage('Key saved!', 'success')
     } else {
-      setMessageType('warning')
-      setMessage('The key is not valid.')
+      showMessage('The key is not valid.', 'warning', 0)
     }
   }
 
@@ -63,6 +96,34 @@ function Options() {
     savePrivateKey(generatePrivateKey())
   }
 
+  function changeRelayURL(i, ev) {
+    setRelays([
+      ...relays.slice(0, i),
+      {url: ev.target.value, policy: relays[i].policy},
+      ...relays.slice(i + 1)
+    ])
+  }
+
+  function toggleRelayPolicy(i, cat) {
+    setRelays([
+      ...relays.slice(0, i),
+      {
+        url: relays[i].url,
+        policy: {...relays[i].policy, [cat]: !relays[i].policy[cat]}
+      },
+      ...relays.slice(i + 1)
+    ])
+  }
+
+  function addNewRelay() {
+    relays.push({
+      url: newRelayURL,
+      policy: {read: true, write: true}
+    })
+    setRelays(relays)
+    setNewRelayURL('')
+  }
+
   return (
     <>
       <header className="header">
@@ -73,47 +134,87 @@ function Options() {
       </header>
       <main>
         <h2>Options</h2>
-        <div className="form-field">
-          <label htmlFor="private-key">Private key:</label>
-          <div className="input-group">
-            <input id="private-key" value={key} onChange={handleKeyChange} />
-            <button onClick={generateRandomPrivateKey}>
-              <DiceIcon /> Generate
-            </button>
-          </div>
-        </div>
-        {permissions?.length > 0 && (
-          <>
-            <h2>Permissions</h2>
-            <table>
-              <thead>
-                <tr>
-                  <th>domain</th>
-                  <th>permissions</th>
-                  <th>condition</th>
-                  <th>since</th>
-                </tr>
-              </thead>
-              <tbody>
-                {permissions.map(({host, level, condition, created_at}) => (
-                  <tr key={host}>
-                    <td>{host}</td>
-                    <td>{getPermissionsString(level)}</td>
-                    <td>{condition}</td>
-                    <td>
-                      {new Date(created_at * 1000)
-                        .toISOString()
-                        .split('.')[0]
-                        .split('T')
-                        .join(' ')}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </>
-        )}
         {message && <Alert message={message} type={messageType} />}
+
+        <section>
+          <div className="form-field">
+            <label htmlFor="private-key">Private key:</label>
+            <div className="input-group">
+              <input id="private-key" value={key} onChange={handleKeyChange} />
+              <button onClick={generateRandomPrivateKey}>
+                <DiceIcon /> Generate
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h3>Permissions</h3>
+          {permissions?.length > 0 ? (
+            <>
+              <table>
+                <thead>
+                  <tr>
+                    <th>domain</th>
+                    <th>permissions</th>
+                    <th>condition</th>
+                    <th>since</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {permissions.map(({host, level, condition, created_at}) => (
+                    <tr key={host}>
+                      <td>{host}</td>
+                      <td>{getPermissionsString(level)}</td>
+                      <td>{condition}</td>
+                      <td>
+                        {new Date(created_at * 1000)
+                          .toISOString()
+                          .split('.')[0]
+                          .split('T')
+                          .join(' ')}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          ) : (
+            <p>(No permissions defined)</p>
+          )}
+        </section>
+
+        <section>
+          <h3>Preferred relays</h3>
+          {relays.map(({url, policy}, i) => (
+            <div key={i} style={{display: 'flex'}}>
+              <input value={url} onChange={changeRelayURL.bind(null, i)} />
+              <label>
+                read
+                <input
+                  type="checkbox"
+                  checked={policy.read}
+                  onChange={toggleRelayPolicy.bind(null, i, 'read')}
+                />
+              </label>
+              <label>
+                write
+                <input
+                  type="checkbox"
+                  checked={policy.write}
+                  onChange={toggleRelayPolicy.bind(null, i, 'write')}
+                />
+              </label>
+            </div>
+          ))}
+          <div style={{display: 'flex'}}>
+            <input
+              value={newRelayURL}
+              onChange={e => setNewRelayURL(e.target.value)}
+              onBlur={addNewRelay}
+            />
+          </div>
+        </section>
       </main>
       <footer>version {manifest.version}</footer>
     </>
