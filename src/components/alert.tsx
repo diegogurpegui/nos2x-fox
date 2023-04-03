fd8a083b21adb20fbf6d211d5dd66bb7e6cf3de0
Diego H. Gurpegui <diego.gurpegui@improvein.com>
2023-04-02 21:55:16 -0300

    Fix asset reference and lint

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,16 +1,16 @@
-import browser from 'webextension-polyfill'
-import React, {useState, useEffect} from 'react'
+import browser from 'webextension-polyfill';
+import React, { useState, useEffect } from 'react';
 
-import InformationCircleIcon from './assets/icons/information-circle-outline.svg'
-import CheckmarkCircleIcon from './assets/icons/checkmark-circle-outline.svg'
-import WarningDiceIcon from './assets/icons/warning-outline.svg'
+import InformationCircleIcon from '../assets/icons/information-circle-outline.svg';
+import CheckmarkCircleIcon from '../assets/icons/checkmark-circle-outline.svg';
+import WarningDiceIcon from '../assets/icons/warning-outline.svg';
 
 interface AlertProps {
-  message: String
-  type: 'info|warning|success'
+  message: String;
+  type: 'info|warning|success';
 }
 
-export function Alert<AlertProps>({message, type}) {
+export function Alert<AlertProps>({ message, type }) {
   return (
     <div className={`alert ${type}`}>
       {type == 'info' ? (
@@ -22,5 +22,5 @@ export function Alert<AlertProps>({message, type}) {
       )}
       {message}
     </div>
-  )
+  );
 }
