a1b1eb0f022e49ed9bca772a6f36fc90519e7d00
Diego H. Gurpegui <diego.gurpegui@improvein.com>
2023-09-03 19:07:46 -0300

    Profile switcher in profile

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,9 @@
 import browser from 'webextension-polyfill';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { getPublicKey, nip19 } from 'nostr-tools';
 import React, { useState, useEffect } from 'react';
 
-import { RelaysConfig } from './types';
+import { ProfilesConfig } from './types';
 import * as Storage from './storage';
 import { truncatePublicKeys } from './common';
 
@@ -15,56 +15,76 @@ function Popup() {
   let [publicKeyHexa, setPublicKeyHexa] = useState('');
   let [publiKeyNIP19, setPublicKeyNIP19] = useState('');
   let [selectedKeyType, setSelectedKeyType] = useState('npub');
+  let [profiles, setProfiles] = useState<ProfilesConfig>({});
 
   useEffect(() => {
     Storage.readActivePrivateKey().then(privateKey => {
       if (privateKey) {
         const pubKey = getPublicKey(privateKey);
         setPublicKeyHexa(pubKey);
-        setPublicKeyNIP19(nip19.npubEncode(pubKey));
-
-        Storage.readActiveRelays().then(relays => {
-          if (relays) {
-            let relaysList: string[] = [];
-            for (let url in relays) {
-              if (relays[url].write) {
-                relaysList.push(url);
-                if (relaysList.length >= 3) break;
-              }
-            }
-            // if (relaysList.length) {
-            //   let nprofileKey = nip19.nprofileEncode({
-            //     pubkey: pubKey,
-            //     relays: relaysList
-            //   })
-            //   keys.current.push(nprofileKey)
-            // }
-          }
-        });
-
-        console.log(`The profile for pubkey '${pubKey}' was loaded.`);
       } else {
         setPublicKeyHexa(null);
         setPublicKeyNIP19(null);
       }
     });
+
+    Storage.readProfiles().then(profiles => {
+      if (profiles) {
+        setProfiles(profiles);
+      }
+    });
   }, []);
+
+  /**
+   * When active public key changes
+   */
+  useEffect(() => {
+    setPublicKeyNIP19(nip19.npubEncode(publicKeyHexa));
+
+    Storage.readActiveRelays().then(relays => {
+      if (relays) {
+        let relaysList: string[] = [];
+        for (let url in relays) {
+          if (relays[url].write) {
+            relaysList.push(url);
+            if (relaysList.length >= 3) break;
+          }
+        }
+      }
+    });
+
+    console.log(`The profile for pubkey '${publicKeyHexa}' was loaded.`);
+  }, [publicKeyHexa]);
 
   function handleKeyTypeSelect(event) {
     setSelectedKeyType(event.target.value);
   }
 
   function goToOptionsPage() {
-    browser.tabs.create({
-      url: browser.runtime.getURL('options.html'),
-      active: true
-    }).then(() => {
-      window.close();
-    });
+    browser.tabs
+      .create({
+        url: browser.runtime.getURL('options.html'),
+        active: true
+      })
+      .then(() => {
+        window.close();
+      });
+  }
+
+  function handleProfileChange(event) {
+    const pubKey = event.target.value;
+    setPublicKeyHexa(pubKey);
+    const profile = profiles[pubKey];
+    if (!profile) {
+      console.warn(`The profile for pubkey '${pubKey}' does not exist.`);
+    }
+    Storage.updateActivePrivateKey(profile.privateKey);
   }
 
   function clipboardCopyPubKey() {
-    navigator.clipboard.writeText(selectedKeyType === 'hex' ? publicKeyHexa : publiKeyNIP19);
+    navigator.clipboard.writeText(
+      selectedKeyType === 'hex' ? publicKeyHexa : publiKeyNIP19
+    );
   }
 
   return (
@@ -86,16 +106,21 @@ function Popup() {
           <div className="public-key">
             <div className="pubkey-show">
               <code>
-                {truncatePublicKeys(selectedKeyType === 'hex' ? publicKeyHexa : publiKeyNIP19)}
+                {truncatePublicKeys(
+                  selectedKeyType === 'hex' ? publicKeyHexa : publiKeyNIP19
+                )}
               </code>
               <button className="button-onlyicon" onClick={clipboardCopyPubKey}>
                 <CopyIcon />
               </button>
             </div>
-            <div className="select key-options">
-              <select value={selectedKeyType} onChange={handleKeyTypeSelect}>
-                <option value="npub">npub</option>
-                <option value="hex">hex</option>
+            <div className="select profile-switch">
+              <select value={publicKeyHexa} onChange={handleProfileChange}>
+                {Object.keys(profiles).map(profilePubKey => (
+                  <option value={profilePubKey} key={profilePubKey}>
+                    {nip19.npubEncode(profilePubKey)}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
@@ -110,4 +135,5 @@ function Popup() {
   );
 }
 
-render(<Popup />, document.getElementById('main'));
+const root = createRoot(document.getElementById('main'));
+root.render(<Popup />);
